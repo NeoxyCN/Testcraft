014f80a97a7d507dcd6fa56de3df207a857478c5
Neoxy <1007050740@qq.com>
2024-01-24 22:44:46 +0800

    use std::format() instead of string  concatenation in LOG.h, this need C++ 20

--- Source/LOG.h
+++ Source/LOG.h
@@ -6,26 +6,26 @@
 #include "Time.h"
 
 namespace LOG {
-	std::string defaultModule = "Main";
+	std::string _defaultModule = "Main";
 
-	void INFO(std::string info, std::string module = defaultModule) { std::cout << "[" << Time::GetCurrentTime() << "]" << "[INFO]" << "[" << module << "]" << info << std::endl; }
-	void WARING(std::string warning, std::string module = defaultModule) { std::cout << "[" << Time::GetCurrentTime() << "]" << "[WARING]" << "[" << module << "]" << warning << std::endl; }
-	void ERROR(std::string error, std::string module = defaultModule) { std::cout << "[" << Time::GetCurrentTime() << "]" << "[ERROR]" << "[" << module << "]" << error << std::endl; }
+	void INFO(std::string info, std::string module = _defaultModule) { std::cout << std::format("[{}][INFO][{}]{}", Time::GetCurrentTime(), module, info) << std::endl; }
+	void WARING(std::string warning, std::string module = _defaultModule) { std::cout << std::format("[{}][WARING][{}]{}", Time::GetCurrentTime(), module, warning) << std::endl; }
+	void ERROR(std::string error, std::string module = _defaultModule) { std::cout << std::format("[{}][ERROR][{}]{}", Time::GetCurrentTime(), module, error) << std::endl; }
 
 	//TODO: colorful print
-	void DEBUG(std::string debug, std::string module = defaultModule) {
-		std::cout << "[" << Time::GetCurrentTime() << "]" << "[DEBUG]" << "[" << module << "]" << debug << std::endl;
+	void DEBUG(std::string debug, std::string module = _defaultModule) {
+		std::cout << std::format("[{}][DEBUG][{}]{}", Time::GetCurrentTime(), module, debug) << std::endl;
 	}
 
-	void IGNORABLE(std::string ignorable, std::string module = defaultModule) {
-		std::cout << "[" << Time::GetCurrentTime() << "]" << "[IGNORABLE]" << "[" << module << "]" << ignorable << std::endl;
+	void IGNORABLE(std::string ignorable, std::string module = _defaultModule) {
+		std::cout << std::format("[{}][IGNORABLE][{}]{}", Time::GetCurrentTime(), module, ignorable) << std::endl;
 	}
 
-	void SUCCESS(std::string success, std::string module = defaultModule) {
-		std::cout << "[" << Time::GetCurrentTime() << "]" << "[SUCCESS]" << "[" << module << "]" << success << std::endl;
+	void SUCCESS(std::string success, std::string module = _defaultModule) {
+		std::cout << std::format("[{}][SUCCESS][{}]{}", Time::GetCurrentTime(), module, success) << std::endl;
 	}
 
-	void FAILED(std::string failed, std::string module = defaultModule) {
-		std::cout << "[" << Time::GetCurrentTime() << "]" << "[FAILED]" << "[" << module << "]" << failed << std::endl;
+	void FAILED(std::string failed, std::string module = _defaultModule) {
+		std::cout << std::format("[{}][FAILED][{}]{}", Time::GetCurrentTime(), module, failed) << std::endl;
 	}
 }
